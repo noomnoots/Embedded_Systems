57bd2c5409da822fc35a4c18933f692214d8080d
Nick Millburn <n340028@gmail.com>
2025-04-21 14:39:36 -0500

    Last commit didn't add final project files despite saying it did. Now they should be there

---
+++ Final project/Jukebox/Jukebox/credits.asm
@@ -0,0 +1,7 @@
+
+/*
+ * credits.asm
+ *
+ * Created: 4/21/2025 2:37:27 PM
+ *  Author: n3400
+ */ 
---
+++ Final project/Jukebox/Jukebox/main.c
@@ -0,0 +1,18 @@
+/*
+ * Jukebox.c
+ *
+ * Created: 4/21/2025 2:30:16 PM
+ * Author : n3400
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
